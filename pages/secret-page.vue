7808697e00b5a88e1164f45ac5b23139d896cc43
tiger <tiger@kittycat.homes>
2024-02-18 22:23:56 +0100

    ugh

--- pages/secret-page.vue
+++ pages/secret-page.vue
@@ -6,6 +6,7 @@
           post.title
         }}</PageTitle>
         <div class="text-neutral p-5 flex flex-col gap-5">
+        <p class="font-sans">help....</p>
           <!-- <Navigation v-if="post._path && (post.title != 'lipu sona mi pi toki pona' && !post._path.includes('translate'))" :path="post._path" :lang="post._path.slice(0,3)" /> -->
           <ContentRenderer :value="post" class="space-y-2" />
           <Navigation v-if="post._path && (post.title != 'lipu sona mi pi toki pona' && !post._path.includes('translate'))" :path="post._path" :lang="post._path.slice(0,3)" />
