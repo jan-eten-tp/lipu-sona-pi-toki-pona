ea70cd48dd22c3f58fc58f20d61bad7a52bace2f
tiger <tiger@kittycat.homes>
2024-02-19 03:40:27 +0100

    added description :o

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,13 +1,19 @@
 <template>
   <article class="w-full flex items-center justify-center flex-col pb-40">
     <div class="md:w-2/3 w-screen">
-      <PageTitle>{{
-        post.title
-      }}</PageTitle>
+      <PageTitle>{{ post.title }}</PageTitle>
       <div class="text-neutral p-5 flex flex-col gap-5">
         <!-- <Navigation v-if="post._path && (post.title != 'lipu sona mi pi toki pona' && !post._path.includes('translate'))" :path="post._path" :lang="post._path.slice(0,3)" /> -->
         <ContentRenderer :value="post" class="space-y-2" />
-        <Navigation v-if="post._path && (post.title != 'lipu sona mi pi toki pona' && !post._path.includes('translate'))" :path="post._path" :lang="post._path.slice(0,3)" />
+        <Navigation
+          v-if="
+            post._path &&
+            post.title != 'lipu sona mi pi toki pona' &&
+            !post._path.includes('translate')
+          "
+          :path="post._path"
+          :lang="post._path.slice(0, 3)"
+        />
       </div>
     </div>
   </article>
@@ -16,9 +22,17 @@
 <script setup lang="ts">
 import { useRoute } from "vue-router";
 
-const post = await queryContent(useRoute().path).findOne(); 
+const post = await queryContent(useRoute().path).findOne();
 
-useHead({
-  title: post.title,
-});
+if ((useRoute().path = "/en")) {
+  useHead({
+    title: post.title,
+    meta: [{ name: "description", content: "this lipu can fit so much sona" }],
+  });
+} else {
+  useHead({
+    title: post.title,
+    meta: [{ name: "description", content: "" }],
+  });
+}
 </script>
