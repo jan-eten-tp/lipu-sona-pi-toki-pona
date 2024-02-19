ec2357e399795367659dd87a628663108e24d71a
tiger <tiger@kittycat.homes>
2024-02-19 03:58:35 +0100

    changed the index page. now the silly secret page is unneeded.

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,5 @@
 <template>
-  <article class="w-full flex items-center justify-center flex-col pb-40">
+  <div class="w-full flex items-center justify-center flex-col pb-40">
     <div class="md:w-2/3 w-screen">
       <PageTitle>{{ post.title }}</PageTitle>
       <div class="text-neutral p-5 flex flex-col gap-5">
@@ -16,7 +16,7 @@
         />
       </div>
     </div>
-  </article>
+  </div>
 </template>
 
 <script setup lang="ts">
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <div></div>
+  <div class="w-full flex items-center justify-center flex-col pb-40">
+    <div class="md:w-2/3 w-screen">
+      <PageTitle>lipu sona mi pi toki pona</PageTitle>
+      <div class="text-neutral p-5 flex flex-col gap-5">
+        <h2 class="md:text-4xl text-3xl text-primary font-display">toki</h2>
+        <LanguageList lang="" />
+      </div>
+    </div>
+  </div>
 </template>
-
-<script setup lang="ts">
-navigateTo('/en')
-</script>
